8f488672c70494623365c0c90d60cbccee87cfc0
Zhenya Kaidashov <zhenya.kaidashov@gmail.com>
2018-10-22 09:32:09 +0300

    Add info logging of books manipulations: create/edit/delete

--- book/views.py
+++ book/views.py
@@ -1,8 +1,12 @@
+import logging
+
 from django.shortcuts import render, redirect, reverse
 from django.views.generic import View
 
 from book.models import Book
 from book.forms import BookForm
+
+logger = logging.getLogger('book')
 
 
 def index(request):
@@ -21,6 +25,7 @@ class BookCreate(View):
 
         if bound_form.is_valid():
             book = bound_form.save()
+            logger.info(f'Create new book: {book.title}')
             return redirect(book)
 
         return render(request, 'book/create.html', {'form': bound_form})
@@ -39,6 +44,7 @@ class BookUpdate(View):
 
         if bound_form.is_valid():
             updated_book = bound_form.save()
+            logger.info(f'Update book: {updated_book.title}')
             return redirect(updated_book)
 
         return render(request, 'book/update.html', {'form': bound_form, 'book': book})
@@ -53,4 +59,5 @@ class BookDelete(View):
     def post(self, request, book_id):
         book = Book.objects.get(id=book_id)
         book.delete()
+        logger.info(f'Delete book: {book.title}')
         return redirect(reverse('book_index_url'))
--- bookstore/settings.py
+++ bookstore/settings.py
@@ -128,3 +128,20 @@ STATIC_URL = '/assets/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/assets'),
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'book': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+    },
+}
